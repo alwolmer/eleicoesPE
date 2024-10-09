a8060d53c2bd1e49d03959bce342a9a5737dcd38
alwolmer <arthur.wolmer@gmail.com>
2024-10-08 22:12:34 -0300

    refactor: reorganized the data necessary for rendering based on new pipeline

--- app/app.py
+++ app/app.py
@@ -12,6 +12,8 @@ import requests
 # Fetching the 'DEV' environment variable (returns None if not found)
 prod_mode = os.getenv('PROD', '0')  # Default to '0' if not set
 
+UF = 'PE' # Hardcode for now
+
 # Convert the value to integer or boolean as needed
 prod = int(prod_mode) == 1
 
@@ -24,33 +26,18 @@ else:
 @st.cache_data
 def load_data():
    
-    cand_PE_abrev = pd.read_csv(fr'{data_path}cand_2022_PE_abrev.csv')
-    voto_PE_nominal = pd.read_csv(fr'{data_path}votacao_mun_2022_PE_nominal.csv')
-    voto_PE_valido = pd.read_csv(fr'{data_path}votacao_mun_2022_PE_valido.csv')
-    malha_PE_mun = gpd.read_file(fr'{data_path}malha_PE_mun.geojson')
+    cand_UF_abrev = pd.read_csv(fr'{data_path}cand_abrev_{UF}_2022.csv')
+    voto_mun_valido = pd.read_csv(fr'{data_path}voto_mun_valido_{UF}_2022.csv')
+    voto_mun_valido_total = pd.read_csv(fr'{data_path}voto_mun_valido_{UF}_total_2022.csv')
+    malha_UF_mun = gpd.read_file(fr'{data_path}malha_{UF}_mun.geojson')
     
-    voto_PE_nominal['CD_MUN'] = voto_PE_nominal['CD_MUN'].astype('Int64')
-    voto_PE_valido['CD_MUN'] = voto_PE_valido['CD_MUN'].astype('Int64')
-    malha_PE_mun['CD_MUN'] = malha_PE_mun['CD_MUN'].astype('Int64')
+    voto_mun_valido['CD_MUN'] = voto_mun_valido['CD_MUN'].astype('Int64')
+    voto_mun_valido_total['CD_MUN'] = voto_mun_valido_total['CD_MUN'].astype('Int64')
+    malha_UF_mun['CD_MUN'] = malha_UF_mun['CD_MUN'].astype('Int64')
     
-    return cand_PE_abrev, voto_PE_nominal, voto_PE_valido, malha_PE_mun
+    return cand_UF_abrev, voto_mun_valido, voto_mun_valido_total, malha_UF_mun
 
-# @st.cache_data
-# def preload_images(image_folder=None):
-#     images = {}
-#     if image_folder is None:
-#         for sq_cand in cand_sq_with_images:
-#             image_path = f"https://github.com/<username>/<repository>/blob/main/foto_cand2022_PE_div/FPE{sq_cand}_div.jpg"
-#             images[sq_cand] = Image.open(image_path)
-#     else:
-#         for filename in os.listdir(image_folder):
-#             if filename.endswith(".jpg") or filename.endswith(".jpeg"):
-#                 sq_cand = filename.split('_')[0].replace("FPE", "")
-#                 image_path = os.path.join(image_folder, filename)
-#                 images[sq_cand] = Image.open(image_path)
-#     return images
-
-cand_PE_abrev, voto_PE_nominal, voto_PE_valido, malha_PE_mun = load_data()
+cand_UF_abrev, voto_mun_valido, voto_mun_valido_total, malha_UF_mun = load_data()
 
 # if dev:
 #     preloaded_images = preload_images(f'{data_path}foto_cand2022_PE_div')
@@ -75,7 +62,7 @@ sit_elec_key = {
 }
 
 selected_cargo_turno = st.sidebar.selectbox('Escolha o cargo', cargo_turno_key.keys())
-cand_selected_cargo_turno = cand_PE_abrev[(cand_PE_abrev['CD_CARGO'] == cargo_turno_key[selected_cargo_turno][0]) & (cand_PE_abrev['NR_TURNO'] == cargo_turno_key[selected_cargo_turno][1]) & ((cand_PE_abrev['CD_SITUACAO_CANDIDATURA'] == 12))] # 12 means able candidate, who actually did run
+cand_selected_cargo_turno = cand_UF_abrev[(cand_UF_abrev['CD_CARGO'] == cargo_turno_key[selected_cargo_turno][0]) & (cand_UF_abrev['NR_TURNO'] == cargo_turno_key[selected_cargo_turno][1]) & ((cand_UF_abrev['CD_SITUACAO_CANDIDATURA'] == 12))] # 12 means able candidate, who actually did run
 
 partido_list = ['TODOS'] + sorted(cand_selected_cargo_turno['SG_PARTIDO'].unique().tolist())
 selected_partido = st.sidebar.selectbox('Escolha o partido', partido_list)
@@ -86,7 +73,7 @@ else:
     candidates = cand_selected_cargo_turno[cand_selected_cargo_turno['SG_PARTIDO'] == selected_partido]['NM_URNA_CANDIDATO'].sort_values()
 selected_candidate = st.sidebar.selectbox('Escolha o candidato', candidates)
 
-cand_details = cand_PE_abrev[(cand_PE_abrev['CD_CARGO'] == cargo_turno_key[selected_cargo_turno][0]) & (cand_PE_abrev['NR_TURNO'] == cargo_turno_key[selected_cargo_turno][1]) & (cand_PE_abrev['NM_URNA_CANDIDATO'] == selected_candidate)]
+cand_details = cand_UF_abrev[(cand_UF_abrev['CD_CARGO'] == cargo_turno_key[selected_cargo_turno][0]) & (cand_UF_abrev['NR_TURNO'] == cargo_turno_key[selected_cargo_turno][1]) & (cand_UF_abrev['NM_URNA_CANDIDATO'] == selected_candidate)]
 
 c1 = st.container()
 
@@ -118,16 +105,16 @@ with col2:
             
 
 @st.cache_data
-def generate_voto_display(voto_PE_nominal, voto_PE_valido, _malha_PE_mun, turno_cand, cargo_cand, numero_cand):
-    voto_select = voto_PE_nominal[(voto_PE_nominal['NR_TURNO'] == turno_cand) & 
-                                  (voto_PE_nominal['CD_CARGO'] == cargo_cand) & 
-                                  (voto_PE_nominal['NR_VOTAVEL'] == numero_cand)].copy()
+def generate_voto_display(voto_mun_valido, voto_mun_valido_total, _malha_UF_mun, turno_cand, cargo_cand, numero_cand):
+    voto_select = voto_mun_valido[(voto_mun_valido['NR_TURNO'] == turno_cand) & 
+                                  (voto_mun_valido['CD_CARGO'] == cargo_cand) & 
+                                  (voto_mun_valido['NR_VOTAVEL'] == numero_cand)].copy()
 
     voto_cand_total = voto_select['QT_VOTOS'].sum()
 
     voto_select = voto_select.merge(
-        voto_PE_valido[(voto_PE_valido['NR_TURNO'] == turno_cand) & 
-                       (voto_PE_valido['CD_CARGO'] == cargo_cand)][['CD_MUN', 'QT_VOTOS']], 
+        voto_mun_valido_total[(voto_mun_valido_total['NR_TURNO'] == turno_cand) & 
+                       (voto_mun_valido_total['CD_CARGO'] == cargo_cand)][['CD_MUN', 'QT_VOTOS']], 
         on='CD_MUN', 
         how='left', 
         suffixes=('_CAND_LOCAL', '_VALIDOS_LOCAL')
@@ -138,12 +125,12 @@ def generate_voto_display(voto_PE_nominal, voto_PE_valido, _malha_PE_mun, turno_
     voto_select['INCID_LOCAL'] = np.round(voto_select['QT_VOTOS_CAND_LOCAL'] / voto_cand_total * 100, 2)
 
     # Merge with GeoDataFrame
-    voto_display = gpd.GeoDataFrame(voto_select.merge(_malha_PE_mun, on='CD_MUN', how='outer').fillna(0))
+    voto_display = gpd.GeoDataFrame(voto_select.merge(_malha_UF_mun, on='CD_MUN', how='outer').fillna(0))
     
     return voto_display, voto_cand_total
 
 # Generate and cache voto_display
-voto_display, voto_cand_total = generate_voto_display(voto_PE_nominal, voto_PE_valido, malha_PE_mun, turno_cand, cargo_cand, numero_cand)
+voto_display, voto_cand_total = generate_voto_display(voto_mun_valido, voto_mun_valido_total, malha_UF_mun, turno_cand, cargo_cand, numero_cand)
 
 c2 = st.container()
 
